5b2d4b52859c0158170998187a98f88fddfb9196
maheshwari05.prerana <maheshwari05.prerana@gmail.com>
2023-03-22 01:15:39 +0100

    Sidebar

---
+++ src/Sidebar.js
@@ -0,0 +1,18 @@
+import { AccountCircleIcon } from "@mui/icons-material/AccountCircle";
+import React from 'react';
+import "./Sidebar.css";
+
+function Sidebar() {
+  return (
+  <div className="Sidebar">
+    <div className="sidebar__top">
+        <img src="" alt=""/>
+        <AccountCircleIcon />
+        <h2>Prerana Maheshwari</h2>
+        <h4>maheshwari05.prerana@gmail.com</h4>
+    </div>
+  </div>
+  );
+}
+
+export default Sidebar;
