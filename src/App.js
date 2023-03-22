9f28b268406b93f12c701992ec327bbd6003d103
maheshwari05.prerana <maheshwari05.prerana@gmail.com>
2023-03-22 01:04:13 +0100

    Build Header

--- src/App.js
+++ src/App.js
@@ -5,9 +5,6 @@ import Header from "./Header";
 function App() {
   return (
     <div className="app">
-      <h1>Lets build a LinkedIn CLONE</h1>
-
-      {/* Header */}
       <Header />
 
       {/* App Body */}
