c6c5895d950fda19f1ff7f2c90eefa076230a861
maheshwari05.prerana <maheshwari05.prerana@gmail.com>
2023-03-23 23:31:29 +0100

    Sidebar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -3,16 +3,45 @@ import React from 'react';
 import "./Sidebar.css";
 
 function Sidebar() {
+
+  const recentItem = (topic) => (
+    <div className="sidebar__recentItem">
+      <span className="sidebar__hash">#</span>
+      <p>{topic}</p>
+
+    </div>
+  );
+
   return (
   <div className="Sidebar">
     <div className="sidebar__top">
-        <img src="" alt=""/>
+        <img src="https://d33wubrfki0l68.cloudfront.net/8d79b89491cf9ef958be1386776dc937d39266be/1a630/img/particle_background.jpg" alt=""/>
         <Avatar className="sidebar__avatar" />
-        <h2>Prerana Maheshwari</h2>
-        <h4>maheshwari05.prerana@gmail.com</h4>
+        <h2>Prerana </h2>
+        <h4>prerana@mattermeaning.com</h4>
+    </div>
+
+    <div className="sidebar__stats">
+      <div className="sidebar__stat">
+        <p>Who viewed you</p>
+        <p className="sidebar__statNumber">2,543</p>
+      </div>
+      <div className="sidebar__stat">
+        <p>Views on post</p>
+        <p className="sidebar__statNumber">2,448</p>
+
+      </div>
+    </div>
+
+    <div className="sidebar__bottom">
+      <p>Recent</p>
+      {recentItem("reactjs")}
+      {recentItem("design")}
+      {recentItem("developer")}
     </div>
   </div>
   );
 }
+
 
 export default Sidebar;
