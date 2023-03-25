8876e6577df491c198b09fa43b17545a70a15cde
maheshwari05.prerana <maheshwari05.prerana@gmail.com>
2023-03-25 01:12:59 +0100

    Input option not visible

--- src/Feed.js
+++ src/Feed.js
@@ -1,6 +1,11 @@
 import React from "react";
 import "./Feed.css";
 import CreateIcon from '@mui/icons-material/Create';
+import inputOption from "./InputOption.css";
+import ImageIcon from '@mui/icons-material/Image';
+import SubscriptionsIcon from '@mui/icons-material/Subscriptions';
+import EventNoteIcon from '@mui/icons-material/EventNote';
+import CalendarViewDayIcon from '@mui/icons-material/CalendarViewDay';
 
 
 function Feed() {
@@ -15,6 +20,10 @@ function Feed() {
             </form>
         </div>
         <div className="feed__inputOptions">
+            <inputOption Icon={ImageIcon} title="Photo" color="#70B5f9"/>
+            <inputOption Icon={SubscriptionsIcon} title="Video" color="#E7A33E"/>
+            <inputOption Icon={EventNoteIcon} title="Event" color="#C0CBCD"/>
+            <inputOption Icon={CalendarViewDayIcon} title="write article" color="#7FC15E"/>
         </div>
     </div>
 </div>
