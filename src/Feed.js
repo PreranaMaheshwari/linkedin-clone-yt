55ebf33ee3aefdde6e483be803387a9a7127c76a
maheshwari05.prerana <maheshwari05.prerana@gmail.com>
2023-03-27 01:59:25 +0200

    Fixed Input Option error

--- src/Feed.js
+++ src/Feed.js
@@ -1,15 +1,17 @@
 import React from "react";
 import "./Feed.css";
 import CreateIcon from '@mui/icons-material/Create';
-import inputOption from "./InputOption.css";
+import InputOption from "./inputOption";
 import ImageIcon from '@mui/icons-material/Image';
 import SubscriptionsIcon from '@mui/icons-material/Subscriptions';
 import EventNoteIcon from '@mui/icons-material/EventNote';
 import CalendarViewDayIcon from '@mui/icons-material/CalendarViewDay';
+import Post from "./Post";
 
 
 function Feed() {
-  return (<div className="feed">
+  return (
+  <div className="feed">
     <div className="feed__inputContainer">
         <div className="feed__input">
              <CreateIcon />
@@ -20,12 +22,15 @@ function Feed() {
             </form>
         </div>
         <div className="feed__inputOptions">
-            <inputOption Icon={ImageIcon} title="Photo" color="#70B5f9"/>
-            <inputOption Icon={SubscriptionsIcon} title="Video" color="#E7A33E"/>
-            <inputOption Icon={EventNoteIcon} title="Event" color="#C0CBCD"/>
-            <inputOption Icon={CalendarViewDayIcon} title="write article" color="#7FC15E"/>
+            <InputOption Icon={ImageIcon} title="Photo" color="#70B5f9"/>
+            <InputOption Icon={SubscriptionsIcon} title="Video" color="#E7A33E"/>
+            <InputOption Icon={EventNoteIcon} title="Event" color="#C0CBCD"/>
+            <InputOption Icon={CalendarViewDayIcon} title="Write article" color="#7FC15E"/>
         </div>
     </div>
+
+    <Post name='Prerana' description='This is a test'
+    message='WOW this worked' />
 </div>
 );
 }
