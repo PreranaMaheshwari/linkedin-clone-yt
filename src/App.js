9d199c1b36d7ba83ff1f5945dce5c0e85aa535c0
maheshwari05.prerana <maheshwari05.prerana@gmail.com>
2023-04-09 01:41:53 +0200

    userSlice

--- src/App.js
+++ src/App.js
@@ -4,12 +4,12 @@ import Feed from "./Feed";
 import Header from "./Header";
 import Sidebar from './Sidebar';
 
+
 function App() {
   return (
     <div className="app">
       <Header />
 
-      {/* App Body */}
       <div className="app__body">
        <Sidebar />
        <Feed />
